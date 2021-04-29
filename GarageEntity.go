cd51fb6af1433511f913f3e82a3403b41874e28a
Jordan Guada <i7solar@hotmail.com>
2021-04-29 02:37:57 -0400

    0.1 - Request skeleton and parser created.

---
+++ GarageEntity.go
@@ -0,0 +1,7 @@
+package main
+
+type ParkingEntity struct {
+	total,
+
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	_ "log"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello World")
+func Check(err error){
+	if err != nil {
+		panic(err)
+		return
+	}
+}
+
+func ParseGarageRequest(GarageIdentity string) {
+
+	fmt.Println(GarageIdentity)
+}
+
+
+
+func GarageRequest(){
+	///////
+	request, err := http.Get("https://secure.parking.ucf.edu/GarageCount/")
+	Check(err)
+	////////
+
+	doc, _ := goquery.NewDocumentFromReader(request.Body)
+	doc.Find("tr.dxgvDataRow_DevEx").Each(func(i int, s *goquery.Selection) {
+		GarageIdentity := s.Find("td").Text()
+		if i == 1{
+			ParseGarageRequest(GarageIdentity)
+		}
+	})
+}
+func main() {
+	/////////////////////////////////////////////
+	fmt.Println("UCF GARAGE SCRAPER V1")
+	/////////////////////////////////////////////
+
+	GarageRequest()
 }
